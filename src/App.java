bd2d7d4038877963034b6219a1a4ea8870818e86
Atila jr <atilla.junior33@gmail.com>
2023-01-10 11:29:56 -0300

    Implementaçao da interface

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import util.ProductPredicate;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("==========================================");
+
+    List<Product> list = new ArrayList<>();
+    list.add(new Product("Tv", 900.00));
+    list.add(new Product("Mouse", 50.00));
+    list.add(new Product("Tablet", 350.50));
+    list.add(new Product("HD Case", 80.90));
+
+    list.removeIf(new ProductPredicate());
+
+    for (Product p : list) {
+      System.out.println(p);
+    }
+
+    System.out.println("==========================================");
+  }
+}
